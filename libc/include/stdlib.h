0c7e89ab6056ff77e980f797c2d26337b37772cc
Jorne De Smedt <jornedesmedt@hotmail.com>
2018-04-13 18:52:23 +0200

    Added some stuff to stdlib.h

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -2,12 +2,39 @@
 #define __POSIX_STDLIB_H__
 
 #include <sys/cdefs.h>
+#include <stddef.h>
+#include <sys/wait.h>
+
+#define EXIT_SUCCESS 0;
+#define EXIT_FAILURE -1;
+#define RAND_MAX 0x7FFFFFFF;
+#define MB_CUR_MAX 1;
+
+typedef struct div {
+    int quot;
+    int rem;
+} div_t;
+
+typedef struct ldiv {
+    long quot;
+    long rem;
+} ldiv_t;
+
+typedef struct lldiv {
+    long long quot;
+    long long rem;
+} lldiv_t;
+
 #ifdef __cplusplus
 extern "C"{
 #endif
 
 __attribute__((__noreturn__))
+void _Exit(int);
+long a641(const char *);
 void abort(void);
+void abs(int);
+void atexit(int);
 
 #ifdef __cplusplus
 }
